93aeec8570f3a41710162fadc206ad3665083e49
Freddy Chiron <fchiron.ext@orange.com>
2016-02-23 00:53:01 +0100

    trying to move responsability

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -16,66 +16,119 @@ public class TennisGame1 implements TennisGame {
 	private static final String FIFTEEN_DISPLAY = "Fifteen";
 	private static final String LOVE_DISPLAY = "Love";
 	private static final String PLAYER1 = "player1";
-	private int firstPlayerScore = 0;
-	private int secondPlayerScore = 0;
+	private Player firstPlayer;
+	private Player secondPlayer;
 
 	public TennisGame1() {
+		firstPlayer = new Player(PLAYER1);
+		secondPlayer = new Player(PLAYER2);
 	}
 
 	public void wonPoint(String playerName) {
-		if (playerName == PLAYER1)
-			firstPlayerScore += 1;
-		else
-			secondPlayerScore += 1;
+		if (PLAYER1.equals(playerName)) {
+			firstPlayer.score();
+		} else {
+			secondPlayer.score();
+		}
 	}
 
 	public String getScore() {
 
-		boolean isEquality = firstPlayerScore == secondPlayerScore;
-		if (isEquality) {
-			return getScoreWhenEquality(firstPlayerScore);
+		if (firstPlayer.isEqualityWith(secondPlayer)) {
+			return getScoreWhenEquality(firstPlayer.getScore());
 		}
 
-		boolean isAdvantageOrWin = firstPlayerScore > FORTY || secondPlayerScore > FORTY;
+		boolean isAdvantageOrWin = firstPlayer.getScore() > FORTY || secondPlayer.getScore() > FORTY;
 		if (isAdvantageOrWin) {
-			return getScoreWhenAdvantageOrWin(firstPlayerScore, secondPlayerScore);
+			return getScoreWhenAdvantageOrWin();
 		}
 
-		return getScoreWhenOtherCases(firstPlayerScore, secondPlayerScore);
+		return getScoreWhenOtherCases();
 	}
 
 	public String getScoreWhenEquality(int playersScore) {
-		if (playersScore > THIRTY) {
+		if (firstPlayer.getScore() > THIRTY) {
 			return DEUCE;
 		}
-		return getScoreDisplay(playersScore) + SCORE_SEPARATOR + ALL;
+		return firstPlayer.getScoreDisplay() + SCORE_SEPARATOR + ALL;
 
 	}
 
-	public String getScoreWhenAdvantageOrWin(int firstPlayerScore, int secondPlayerScore) {
-		String leadingOrWinningPlayer = firstPlayerScore - secondPlayerScore > 0 ? PLAYER1 : PLAYER2;
-		String advantageOrWinFor = Math.abs(firstPlayerScore - secondPlayerScore) == 1 ? ADVANTAGE : WIN_FOR;
+	public String getScoreWhenAdvantageOrWin() {
+		String leadingOrWinningPlayer = firstPlayer.isLeadingOrWinningVs(secondPlayer) ? firstPlayer.getName()
+				: secondPlayer.getName();
+		String advantageOrWinFor = Math.abs(firstPlayer.getScore() - secondPlayer.getScore()) == 1 ? ADVANTAGE
+				: WIN_FOR;
 		return advantageOrWinFor + leadingOrWinningPlayer;
 
 	}
 
-	public String getScoreWhenOtherCases(int firstPlayerScore, int secondPlayerScore) {
+	public String getScoreWhenOtherCases() {
 
-		return getScoreDisplay(firstPlayerScore) + SCORE_SEPARATOR + getScoreDisplay(secondPlayerScore);
+		return firstPlayer.getScoreDisplay() + SCORE_SEPARATOR + secondPlayer.getScoreDisplay();
 	}
 
-	public String getScoreDisplay(int score) {
-		switch (score) {
-		case LOVE:
-			return LOVE_DISPLAY;
-		case FIFTEEN:
-			return FIFTEEN_DISPLAY;
-		case THIRTY:
-			return THIRTY_DISPLAY;
-		case FORTY:
-			return FORTY_DISPLAY;
-		default:
-			return EMPTY;
+	public class Player {
+		private String name;
+		private Score score;
+
+		public Player(String name) {
+			this.name = name;
+			this.score = new Score();
 		}
+
+		public void score() {
+			this.score.addPoint();
+		}
+
+		public int getScore() {
+			return score.value;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public boolean isLeadingOrWinningVs(Player otherPlayer) {
+			return this.getScore() - otherPlayer.getScore() > 0;
+		}
+
+		public boolean isEqualityWith(Player otherPlayer) {
+			return this.getScore() == otherPlayer.getScore();
+		}
+
+		public String getScoreDisplay() {
+			return score.getDisplay();
+		}
+
+		private class Score {
+
+			int value;
+
+			public Score() {
+				value = 0;
+			}
+
+			public void addPoint() {
+				value++;
+
+			}
+
+			public String getDisplay() {
+				switch (value) {
+				case LOVE:
+					return LOVE_DISPLAY;
+				case FIFTEEN:
+					return FIFTEEN_DISPLAY;
+				case THIRTY:
+					return THIRTY_DISPLAY;
+				case FORTY:
+					return FORTY_DISPLAY;
+				default:
+					return EMPTY;
+				}
+			}
+		}
+
 	}
 }
