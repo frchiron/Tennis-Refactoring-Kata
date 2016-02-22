0935000a88e3d4f2d5e2c6d2392f3bcc72e4cd35
Freddy Chiron <fchiron.ext@orange.com>
2016-02-22 21:37:10 +0100

    add methods for equality and advantage/win

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -20,57 +20,75 @@ public class TennisGame1 implements TennisGame {
 	}
 
 	public String getScore() {
+
+		boolean isEquality = firstPlayerScore == secondPlayerScore;
+		if (isEquality) {
+			return getScoreWhenEquality();
+		}
+
+		boolean isAdvantageOrWin = firstPlayerScore >= 4 || secondPlayerScore >= 4;
+		if (isAdvantageOrWin) {
+			return getScoreWhenAdvantageOrWin();
+		}
+
 		String score = "";
-		int tempScore = 0;
-		if (firstPlayerScore == secondPlayerScore) {
-			switch (firstPlayerScore) {
+		for (int i = 1; i < 3; i++) {
+			int tempScore = 0;
+			if (i == 1)
+				tempScore = firstPlayerScore;
+			else {
+				score += "-";
+				tempScore = secondPlayerScore;
+			}
+			switch (tempScore) {
 			case 0:
-				score = "Love-All";
+				score += LOVE;
 				break;
 			case 1:
-				score = "Fifteen-All";
+				score += FIFTEEN;
 				break;
 			case 2:
-				score = "Thirty-All";
+				score += THIRTY;
 				break;
-			default:
-				score = "Deuce";
+			case 3:
+				score += FORTY;
 				break;
+			}
+		}
+		return score;
+	}
 
-			}
-		} else if (firstPlayerScore >= 4 || secondPlayerScore >= 4) {
-			int minusResult = firstPlayerScore - secondPlayerScore;
-			if (minusResult == 1)
-				score = "Advantage player1";
-			else if (minusResult == -1)
-				score = "Advantage player2";
-			else if (minusResult >= 2)
-				score = "Win for player1";
-			else
-				score = "Win for player2";
-		} else {
-			for (int i = 1; i < 3; i++) {
-				if (i == 1)
-					tempScore = firstPlayerScore;
-				else {
-					score += "-";
-					tempScore = secondPlayerScore;
-				}
-				switch (tempScore) {
-				case 0:
-					score += LOVE;
-					break;
-				case 1:
-					score += FIFTEEN;
-					break;
-				case 2:
-					score += THIRTY;
-					break;
-				case 3:
-					score += FORTY;
-					break;
-				}
-			}
+	public String getScoreWhenAdvantageOrWin() {
+		String score = "";
+		int minusResult = firstPlayerScore - secondPlayerScore;
+		if (minusResult == 1)
+			score = "Advantage player1";
+		else if (minusResult == -1)
+			score = "Advantage player2";
+		else if (minusResult >= 2)
+			score = "Win for player1";
+		else
+			score = "Win for player2";
+
+		return score;
+	}
+
+	public String getScoreWhenEquality() {
+		String score;
+		switch (firstPlayerScore) {
+		case 0:
+			score = "Love-All";
+			break;
+		case 1:
+			score = "Fifteen-All";
+			break;
+		case 2:
+			score = "Thirty-All";
+			break;
+		default:
+			score = "Deuce";
+			break;
+
 		}
 		return score;
 	}
