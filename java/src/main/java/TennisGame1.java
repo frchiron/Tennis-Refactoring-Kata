5e48614cd7aba3d29fee3a102bac8a2a76cd430d
Freddy Chiron <fchiron.ext@orange.com>
2016-02-22 23:51:28 +0100

    final refactor

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,22 +1,20 @@
-
 public class TennisGame1 implements TennisGame {
 
 	private static final String PLAYER2 = "player2";
 	private static final String WIN_FOR = "Win for ";
 	private static final String ADVANTAGE = "Advantage ";
-	private static final int _4 = 4;
-	private static final int _3 = 3;
-	private static final int _0 = 0;
-	private static final int _1 = 1;
-	private static final int _2 = 2;
+	private static final int FORTY = 3;
+	private static final int LOVE = 0;
+	private static final int FIFTEEN = 1;
+	private static final int THIRTY = 2;
 	private static final String DEUCE = "Deuce";
 	private static final String ALL = "All";
 	private static final String EMPTY = "";
 	private static final String SCORE_SEPARATOR = "-";
-	private static final String FORTY = "Forty";
-	private static final String THIRTY = "Thirty";
-	private static final String FIFTEEN = "Fifteen";
-	private static final String LOVE = "Love";
+	private static final String FORTY_DISPLAY = "Forty";
+	private static final String THIRTY_DISPLAY = "Thirty";
+	private static final String FIFTEEN_DISPLAY = "Fifteen";
+	private static final String LOVE_DISPLAY = "Love";
 	private static final String PLAYER1 = "player1";
 	private int firstPlayerScore = 0;
 	private int secondPlayerScore = 0;
@@ -38,7 +36,7 @@ public class TennisGame1 implements TennisGame {
 			return getScoreWhenEquality(firstPlayerScore);
 		}
 
-		boolean isAdvantageOrWin = firstPlayerScore >= _4 || secondPlayerScore >= _4;
+		boolean isAdvantageOrWin = firstPlayerScore > FORTY || secondPlayerScore > FORTY;
 		if (isAdvantageOrWin) {
 			return getScoreWhenAdvantageOrWin(firstPlayerScore, secondPlayerScore);
 		}
@@ -46,40 +44,38 @@ public class TennisGame1 implements TennisGame {
 		return getScoreWhenOtherCases(firstPlayerScore, secondPlayerScore);
 	}
 
-	public String getScoreWhenOtherCases(int firstPlayerScore, int secondPlayerScore) {
-
-		return convertPlayerScoreToLitteral(firstPlayerScore) + SCORE_SEPARATOR
-				+ convertPlayerScoreToLitteral(secondPlayerScore);
-	}
-
-	public String convertPlayerScoreToLitteral(int playerScore) {
-		switch (playerScore) {
-		case _0:
-			return LOVE;
-		case _1:
-			return FIFTEEN;
-		case _2:
-			return THIRTY;
-		case _3:
-			return FORTY;
-		default:
-			return EMPTY;
+	public String getScoreWhenEquality(int playersScore) {
+		if (playersScore > THIRTY) {
+			return DEUCE;
 		}
+		return getScoreDisplay(playersScore) + SCORE_SEPARATOR + ALL;
+
 	}
 
 	public String getScoreWhenAdvantageOrWin(int firstPlayerScore, int secondPlayerScore) {
-		int scoreDifference = firstPlayerScore - secondPlayerScore;
-		String leadingOrWinningPlayer = scoreDifference > 0 ? PLAYER1 : PLAYER2;
-		String advantageOrWinFor = Math.abs(scoreDifference) == 1 ? ADVANTAGE : WIN_FOR;
+		String leadingOrWinningPlayer = firstPlayerScore - secondPlayerScore > 0 ? PLAYER1 : PLAYER2;
+		String advantageOrWinFor = Math.abs(firstPlayerScore - secondPlayerScore) == 1 ? ADVANTAGE : WIN_FOR;
 		return advantageOrWinFor + leadingOrWinningPlayer;
 
 	}
 
-	public String getScoreWhenEquality(int playersScore) {
-		if (playersScore > _2) {
-			return DEUCE;
-		}
-		return convertPlayerScoreToLitteral(playersScore) + SCORE_SEPARATOR + ALL;
+	public String getScoreWhenOtherCases(int firstPlayerScore, int secondPlayerScore) {
 
+		return getScoreDisplay(firstPlayerScore) + SCORE_SEPARATOR + getScoreDisplay(secondPlayerScore);
+	}
+
+	public String getScoreDisplay(int score) {
+		switch (score) {
+		case LOVE:
+			return LOVE_DISPLAY;
+		case FIFTEEN:
+			return FIFTEEN_DISPLAY;
+		case THIRTY:
+			return THIRTY_DISPLAY;
+		case FORTY:
+			return FORTY_DISPLAY;
+		default:
+			return EMPTY;
+		}
 	}
 }
